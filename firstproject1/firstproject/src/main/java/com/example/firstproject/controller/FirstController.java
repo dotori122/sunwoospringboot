810496f861dcf736f81d6c33e1ce7fc508e37084
dotori122 <tjsdnda122@gmail.com>
2026-04-05 23:54:21 +0900

    0401 spring

--- firstproject1/firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject1/firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -12,14 +12,14 @@ public class FirstController {
 
     public String niceToMeetYou(Model model) { //메서드 작성 // model 객체 받아 오기
         // model 객체가 "홍팍" 값을 "username"에 연결해 웹 브라우저로 보냄
-        model.addAttribute("username", "hongpark"); // 모델 변수 등록
-        // model.addAttribute : 변숫값을 "변수명"이라는 이름으로 추가 // "홍팍" → "hongpark" 수정
+        model.addAttribute("username", "EJ"); // 모델 변수 등록
+        // model.addAttribute : 변숫값을 "변수명"이라는 이름으로 추가
         return "greetings"; // greetings.mustache 파일 반환 (뷰 템플릿 페이지 반환)
     }
     @GetMapping("/bye") // URL 요청 접수
 
     public String seeYouNext(Model model) { //메서드 작성 // model 객체 받아 오기
-            model. addAttribute("nickname", "홍길동"); // 모델 변수 등록하기
+            model. addAttribute("nickname", "은진"); // 모델 변수 등록하기
             return "goodbye"; // goodbye.mustache 파일 반환
 
         }
